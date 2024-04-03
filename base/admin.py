37dce112176a5aebdf0e06a665d7494acdd642df
Edgar1579 <es535965@gmail.com>
2024-04-03 16:24:29 -0500

    index inicial

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from comunidad.models import Usuario
 from configuracion.models import Slider
 # Register your models here.
-admin.site.register(Usuario)
+admin.site.register(Slider)
